08dc02137c75090f8e3d9a151ac95688778f6a22
adanzl <adanzl@163.com>
2018-09-26 23:47:34 +0800

    初始化提交

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
+  }
+
+  /**
+   * showAddTaskPage
+   */
+  public showAddTaskPage() {
+    const modal = this.modalCtrl.create('AddTaskPage');
+    modal.present();
   }
 
 }
